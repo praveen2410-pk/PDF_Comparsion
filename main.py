3aae7b02b8b827bd587c6c0da62fa7f1323853b1
Praveen Kumar Mathiyazhagan <praveenmathi2410@gmail.com>
2023-12-10 00:08:38 +0300

    new

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import pdfplumber
+
+def compare_pdfs(source_folder, target_folder, log_file):
+    source_files = os.listdir(source_folder)
+    target_files = os.listdir(target_folder)
+
+    common_files = set(source_files) & set(target_files)
+
+    with open(log_file, 'w') as log:
+        for filename in common_files:
+            source_path = os.path.join(source_folder, filename)
+            target_path = os.path.join(target_folder, filename)
+
+            if os.path.isfile(source_path) and os.path.isfile(target_path):
+                source_text = extract_text_from_pdf(source_path)
+                target_text = extract_text_from_pdf(target_path)
+
+                if source_text == target_text:
+                    log.write(f"File {filename} is identical.\n")
+                else:
+                    log.write(f"File {filename} is different:\n")
+                    log.write(f"Reason: Text content does not match.\n\n")
+
+def extract_text_from_pdf(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        text = ""
+        for page in pdf.pages:
+            text += page.extract_text()
+        return text
+
+if __name__ == "__main__":
+    source_folder = "path/to/source/folder"
+    target_folder = "path/to/target/folder"
+    log_file = "comparison_log.txt"
+
+    compare_pdfs(source_folder, target_folder, log_file)
